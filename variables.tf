bc54c84aa707f5cd52727df972919e653cfeea40
Aakash Kota <kota15@umd.edu>
2026-02-20 21:45:48 -0500

    Terraform Initialization

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "aws_region" {
+    default = "us-east-1"
+}
+
+variable "vpc_cidr" {
+    default= "10.0.0.0/16"
+}
+
+variable "azs" {
+    default = ["us-east-1a", "us-east-1b"]
+}
+
+variable "instance_type" {
+    default= "t3.micro"
+}
